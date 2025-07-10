9762a9ea2acd84c9c19ca228dc47022505949538
SantiagoColombet <48458914@est.ort.edu.ar>
2025-07-10 08:55:11 -0300

    finish

--- src/Productos.jsx
+++ src/Productos.jsx
@@ -4,12 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Productos({ producto }) {
   return (
-    <div class="card" style={{ width: '18rem' }}>
-        <img src={producto.image} class="card-img-top" alt="..." />
+    <div className="card" style={{ width: '18rem' }}>
+        <img src={producto.image} className="card-img-top" alt="..." />
         <div className="card-body">
         <h5 className="card-title">{producto.title}</h5>
         <p className="card-text">{producto.description}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
     </div>
   )
